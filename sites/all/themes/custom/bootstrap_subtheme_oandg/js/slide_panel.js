a0f011d6fc337a2a0332ad5ff7afc77ac7bfab26
Michael Tripp <ixtlandes@mindspring.com>
2016-06-16 14:09:01 -0400

    secondary menu css

--- sites/all/themes/custom/bootstrap_subtheme_oandg/js/slide_panel.js
+++ sites/all/themes/custom/bootstrap_subtheme_oandg/js/slide_panel.js
@@ -15,29 +15,43 @@
                 jQuery(".container").removeClass().addClass("fluid-container");
                 jQuery(".row").removeClass().addClass("row-fluid");
                 
-                jQuery("li.expanded.dropdown").mouseover(function(){
+                jQuery( "span:contains('Subscribe ')" ).addClass("subscribe");
+                jQuery(".first.expanded.dropdown").css("background-color", "#7F1334" );
+                jQuery( ".subscribe").empty();
+                
+ 
+                 jQuery( "a:contains('Facebook')" ).addClass("facebook");
+                 jQuery( ".facebook").empty();     
+
+                 jQuery( "a:contains('Twitter')" ).addClass("twitter");
+                 jQuery( ".twitter").empty();  
+                
+                jQuery( "span:contains('Gallery')" ).addClass("gallery");
+                jQuery( "span:contains('Locations & Hours')" ).addClass("road");
+                jQuery( "span:contains('Blog')" ).addClass("blog"); 
+   
+                
+                jQuery( "span:contains('Blog')" ).addClass("blog");
+                
+
+                jQuery( "span:contains('blank')" ).addClass("blank");
+                jQuery( "span.blank").empty();
+
+                
+                jQuery("#block-system-main-menu ul.menu.nav li.expanded.dropdown").mouseover(function(){
                     jQuery(this).css("background-color", "#E0D9B8" );
                    /* jQuery("#mason-nav-container").animate({left: '-=150'}, "slow");*/
                 if (jQuery("#mason-nav-container.shrink")[0]){
                     // Do something if class exists
                 } else {
                     jQuery("#mason-nav-container").removeClass("grow").addClass("shrink");
-                }                   
-                   
-                });
-             
-                jQuery("li.expanded.dropdown").mouseleave(function() {
+                }                                                          
+                });            
+                jQuery("#block-system-main-menu ul.menu.nav li.expanded.dropdown").mouseleave(function() {
                        jQuery(this).css("background-color", "#7F1334" );
                       /* jQuery("#mason-nav-container").animate({left: '+=150'}, "slow");*/
-                      jQuery("#mason-nav-container").removeClass("shrink").addClass("grow");
-                /*if (jQuery("#mason-nav-container.shrink")[0]){
-                    jQuery("#mason-nav-container").removeClass("shrink").addClass("grow");
-                } else {
-                    jQuery("#mason-nav-container").removeClass("grow");
-                }  */                 
-                                         
-                      
-                });               
+                      jQuery("#mason-nav-container").removeClass("shrink").addClass("grow");                                                            
+                });                        
               
             // end our js code
   }       
