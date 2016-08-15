71753e0862cc9a96ace2926f5966c35ad9002e84
Michael Tripp <michael@ixtlandesigns.com>
2016-08-15 11:10:55 -0400

    overflow bug fixed

--- sites/all/themes/custom/bootstrap_subtheme_oandg/js/slide_panel.js
+++ sites/all/themes/custom/bootstrap_subtheme_oandg/js/slide_panel.js
@@ -10,10 +10,6 @@
                 var doc = document.documentElement;
                 doc.setAttribute('data-useragent', navigator.userAgent);
 
-                jQuery(window).load(function() {
-                  jQuery('#loading').hide();
-                });
-
                 jQuery( "<div class='ie-holder red'></div>" ).insertBefore( "#block-views-mason-home-page-icon-block");
                 jQuery( "<div class='ie-holder green'></div>" ).insertBefore( "#block-search-form");
                 jQuery( "<div class='ie-holder blue'></div>" ).insertBefore( "#block-system-main-menu");
